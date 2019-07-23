489f359b7608b88000bbceb4e836fccb612cc6fa
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-07-22 21:58:38 -0300

    it's workinngg

--- bot/bot.js
+++ bot/bot.js
@@ -8,17 +8,17 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 const PORT = process.env.PORT || 3000;
 const APP_URL = process.env.APP_URL;
 
-console.log('token' + BOT_TOKEN);
-console.log('porta:' + PORT);
-console.log('url:' + APP_URL);
-
 const bot = new Telegraf(BOT_TOKEN);
 bot.telegram.setWebhook(`${APP_URL}/bot${BOT_TOKEN}`);
 expressApp.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
 /*
  your bot commands and all the other stuff on here ....
 */
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
 // and at the end just start server on PORT
 expressApp.get('/', (req, res) => {
   res.send('Hello World!');
