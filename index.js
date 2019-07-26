67648f8093cf269261f7571f7084961302d70b06
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-07-25 21:57:33 -0300

    Sendding messages logs to me

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ expressApp.use(bot.webhookCallback(`/${BOT_TOKEN}`))
 
 bot.on('text', ctx => {
     //database.saveUserChat()
-    return ctx.reply('new user: ' + `from-id:${ctx.from.id} chat-id:${ctx.chat.id}`)
+    ctx.sendMessage('89797745', 'new user: ' + `from-id:${ctx.from.id} chat-id:${ctx.chat.id}`)
 })
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
