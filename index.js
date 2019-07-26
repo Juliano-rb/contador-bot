d7790c87a7bc0ba9f69133834b8c6e33b249c66c
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-07-25 22:04:20 -0300

    Improoving log

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ expressApp.use(bot.webhookCallback(`/${BOT_TOKEN}`))
 
 bot.on('text', ctx => {
     //database.saveUserChat()
-    bot.telegram.sendMessage('89797745', 'new user: ' + `from-id:${ctx.from.id} chat-id:${ctx.chat.id}`)
+    bot.telegram.sendMessage('89797745', 'new user: ' + `from-id:${ctx.from.id} text:${ctx.text}`)
 })
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
