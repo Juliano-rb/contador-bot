b4fc093946b64e9c4408f95a0bf455d0af71f6eb
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-07-25 21:41:42 -0300

    trying fixing the webhoob setting up proper

--- index.js
+++ index.js
@@ -9,19 +9,12 @@ const PORT = process.env.PORT || 3000
 const APP_URL = process.env.APP_URL
 
 const bot = new Telegraf(BOT_TOKEN)
-expressApp.use(bot.webhookCallback(`/${BOT_TOKEN}`))
-bot.telegram.setWebhook(`${APP_URL}`)
 
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears('bolsonaro', (ctx) => 
-    ctx.reply('https://api.telegram.org/bot{my_bot_token}/setWebhook?url={url_to_send_updates_to}')
-)
+bot.telegram.setWebhook(`${APP_URL}/${BOT_TOKEN}`)
+expressApp.use(bot.webhookCallback(`/${BOT_TOKEN}`))
 
 expressApp.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 expressApp.listen(PORT, () => {
