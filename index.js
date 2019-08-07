47f80cb7e86533f4f7b0c5b838640f19cf94739d
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-08-07 20:41:38 -0300

    Criação do arquivo de rotas e modularização dos comandos do bot para lá

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 const Telegraf = require('telegraf')
+const routes = require('./routes')
 const express = require('express')
 const expressApp = express()
 
@@ -19,50 +20,12 @@ if(! eval(process.env.DEV)){
   expressApp.use(bot.webhookCallback(`/${BOT_TOKEN}`))
 }
 
-bot.on('text', (ctx, next) => {
-    bot.telegram.sendMessage(OWNER_CHAT_ID, 
-      "*New Message:*\n*chat-id*: "+
-      ctx.from.id+"\n"+
-      "*username*: "+ctx.from.username+"\n"+
-      "*text*: _"+ctx.message.text+"_",
-      {parse_mode:'Markdown'}
-    )
-    //console.log(ctx.message.entities)
-    return next(ctx)
-})
+routes(bot)
 
 if(eval(process.env.DEV)){
   console.log("Starting in DEV mode")
   bot.launch()
 }
-
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-
-bot.command('send', (ctx) => {
-  let input = ctx.message.text
-  input = input.substring(input.search(' ')+1)
-
-  let chat_id = input.substring(0,input.search(' '))
-  let message = input.substring(input.search(' ')+1)
-
-  //let params = ctx.message.text.split(" ");
-  console.log(`/send to chat_id:${chat_id}, content:${message}`);
-
-  bot.telegram.sendMessage(chat_id, 
-    message,
-    {parse_mode:'Markdown'}
-  )
-
-  ctx.reply("*Você mandou a mensagem: * " + message + "\n*Para*: " + chat_id,
-  {parse_mode:'Markdown'}
-  )
-})
 
 expressApp.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ routes.js
@@ -0,0 +1,43 @@
+const OWNER_CHAT_ID = '89797745'
+
+module.exports = (bot)=>{
+    bot.on('text', (ctx, next) => {
+        bot.telegram.sendMessage(OWNER_CHAT_ID, 
+        "*New Message:*\n*chat-id*: "+
+        ctx.from.id+"\n"+
+        "*username*: "+ctx.from.username+"\n"+
+        "*text*: _"+ctx.message.text+"_",
+        {parse_mode:'Markdown'}
+        )
+        //console.log(ctx.message.entities)
+        return next(ctx)
+    })
+
+    bot.start((ctx) => ctx.reply('Welcome!'))
+    bot.help((ctx) => ctx.reply('Send me a sticker'))
+    bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+    bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+    bot.command('send', (ctx) => {
+    let input = ctx.message.text
+    input = input.substring(input.search(' ')+1)
+
+    let chat_id = input.substring(0,input.search(' '))
+    let message = input.substring(input.search(' ')+1)
+
+    //let params = ctx.message.text.split(" ");
+    console.log(`/send to chat_id:${chat_id}, content:${message}`);
+
+    bot.telegram.sendMessage(chat_id, 
+        message,
+        {parse_mode:'Markdown'}
+    )
+
+    ctx.reply("*Você mandou a mensagem: * " + message + "\n*Para*: " + chat_id,
+     {parse_mode:'Markdown'}
+    )
+    })
+}
