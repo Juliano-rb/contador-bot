f2b6816dd7a8e18b4b9f53ec06d9fcd4e5e2d569
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-08-07 20:43:07 -0300

    ammend

--- routes.js
+++ routes.js
@@ -22,22 +22,22 @@ module.exports = (bot)=>{
     bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
     bot.command('send', (ctx) => {
-    let input = ctx.message.text
-    input = input.substring(input.search(' ')+1)
+        let input = ctx.message.text
+        input = input.substring(input.search(' ')+1)
 
-    let chat_id = input.substring(0,input.search(' '))
-    let message = input.substring(input.search(' ')+1)
+        let chat_id = input.substring(0,input.search(' '))
+        let message = input.substring(input.search(' ')+1)
 
-    //let params = ctx.message.text.split(" ");
-    console.log(`/send to chat_id:${chat_id}, content:${message}`);
+        //let params = ctx.message.text.split(" ");
+        console.log(`/send to chat_id:${chat_id}, content:${message}`);
 
-    bot.telegram.sendMessage(chat_id, 
-        message,
+        bot.telegram.sendMessage(chat_id, 
+            message,
+            {parse_mode:'Markdown'}
+        )
+
+        ctx.reply("*Você mandou a mensagem: * " + message + "\n*Para*: " + chat_id,
         {parse_mode:'Markdown'}
-    )
-
-    ctx.reply("*Você mandou a mensagem: * " + message + "\n*Para*: " + chat_id,
-     {parse_mode:'Markdown'}
-    )
+        )
     })
 }
