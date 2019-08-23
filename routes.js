0538c3e1106276074f64d5605d16a066cc79347f
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-08-23 19:48:35 -0300

    adds commandArgs middleware This is used to expose the parameters send by user in commands

--- routes.js
+++ routes.js
@@ -1,7 +1,15 @@
 const Main = require('./controllers/main')
 const ProtectedArea = require('./controllers/protectedArea')
+const commandArgs = require('./middlewares/commandArgs')
 
 module.exports = (bot)=> {
+    // by lars graubner, exposes the parameters of the message
+    bot.use(commandArgs())
+
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
     Main(bot)
     ProtectedArea(bot)
+
+   
 }
