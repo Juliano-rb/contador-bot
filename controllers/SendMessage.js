f21b0775610705417696ce157ba4cb42b1031dd8
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-08-23 20:15:53 -0300

    add colors mapping to console text

--- controllers/SendMessage.js
+++ controllers/SendMessage.js
@@ -1,5 +1,14 @@
+const col = {
+    default : '\x1b[0m',
+    yellow : '\x1b[33m',
+    red : '\x1b[31m',
+    blue : '\x1b[34m',
+    magenta : '\x1b[35m'
+
+}
+
 module.exports = (tg, chat_id, message) => {
-    console.log(`*------------\n/send to chat_id:\x1b[31m ${chat_id}\x1b[0m, content:\x1b[31m${message}\x1b[0m\n*------------`
+    console.log(`${col.yellow}*------------\n${col.default}/send to chat_id: ${col.red} ${chat_id}${col.default}\ncontent:\x1b[31m${message}\n${col.yellow}*------------${col.default}`
                 );
 
     tg.sendMessage(chat_id, 
