7140120b75cc464b89605326a36f267a0c39ec5f
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-09-11 20:25:30 -0300

    adiciona menu da cena helloArea

--- src/scenes/helloArea.js
+++ src/scenes/helloArea.js
@@ -1,12 +1,26 @@
 const Scene = require("telegraf/scenes/base");
+const Extra = require("telegraf/extra");
+const Markup = require("telegraf/markup");
 
 const helloArea = new Scene("helloArea");
-helloArea.enter(ctx => ctx.reply("Hi"));
+
+helloArea.enter(ctx => ctx.reply("Bem vindo", helloArea.Menu));
+
+helloArea.command("start", ctx => ctx.reply("Bem vindo", helloArea.Menu));
 
 helloArea.command("admin", ctx => ctx.scene.enter("protectedArea"));
-helloArea.command("/compra", ctx => ctx.scene.enter("newPurchase"));
+helloArea.command("compra", ctx => ctx.scene.enter("newPurchase"));
 
 helloArea.help(ctx => ctx.reply("Send me a sticker"));
 helloArea.on("sticker", ctx => ctx.reply("Gostei"));
 helloArea.on("message", ctx => ctx.reply("E aê meu chapa"));
+
+helloArea.Menu = Markup.keyboard([
+    ["/compra"],
+    ["📢 Admin", "⭐️ Git", "👥 Share"]
+])
+    .oneTime()
+    .resize()
+    .extra();
+
 module.exports = helloArea;
