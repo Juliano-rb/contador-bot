ffcbbca006738410f999e5fdfd8f5400329acaa7
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-09-11 20:36:35 -0300

    putting the keyboards in the right place (in it's scene)

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,13 +1,9 @@
 const logger = require("./middlewares/logger");
 const commandArgs = require("./middlewares/commandArgs");
-const mainMenu = require("./middlewares/mainMenu")
-const newPurchaseMenu = require("./middlewares/newPurchaseMenu");
 
 const Load = bot => {
     bot.use(logger());
     bot.use(commandArgs());
-    bot.use(newPurchaseMenu.init());
-    mainMenu.register(bot);
 };
 
 module.exports = { Load };
