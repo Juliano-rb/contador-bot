6ea10612278fed10de5c41190eb59223aeae1332
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2019-09-11 21:48:19 -0300

    menu commands to CamelCase

--- src/scenes/helloArea.js
+++ src/scenes/helloArea.js
@@ -4,19 +4,19 @@ const Markup = require("telegraf/markup");
 
 const helloArea = new Scene("helloArea");
 
-helloArea.enter(ctx => ctx.reply("Bem vindo", helloArea.Menu));
+helloArea.enter(ctx => ctx.reply("Bem vindo", helloArea.menu));
 
-helloArea.command("start", ctx => ctx.reply("Bem vindo", helloArea.Menu));
+helloArea.command("start", ctx => ctx.reply("Bem vindo", helloArea.menu));
 
-helloArea.command("admin", ctx => ctx.scene.enter("protectedArea"));
-helloArea.command("compra", ctx => ctx.scene.enter("newPurchase"));
+helloArea.hears("📢 Admin", ctx => ctx.scene.enter("protectedArea"));
+helloArea.command("Compra", ctx => ctx.scene.enter("newPurchase"));
 
 helloArea.help(ctx => ctx.reply("Send me a sticker"));
 helloArea.on("sticker", ctx => ctx.reply("Gostei"));
 helloArea.on("message", ctx => ctx.reply("E aê meu chapa"));
 
-helloArea.Menu = Markup.keyboard([
-    ["/compra"],
+helloArea.menu = Markup.keyboard([
+    ["/Compra"],
     ["📢 Admin", "⭐️ Git", "👥 Share"]
 ])
     .oneTime()
